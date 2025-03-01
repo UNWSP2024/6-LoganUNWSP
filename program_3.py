321ee3e798acca6fc7c967e0336837c5a262c18a
LoganUNWSP <lkholmstrom@students.unwsp.edu>
2025-02-28 21:11:54 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,11 +1,35 @@
-# Program #3: Tax Rate
-# A retail company must file a monthly sales tax report listing the total sales for the month, 
-# and the amount of state and county sales tax collected. 
-# The state sales tax rate is 5 percent and the county sales tax rate is 2.5 percent.  
-# Write a program that asks the user to enter the total sales for the month.  
-# From this figure, the application should calculate and display the following:
+#Logan H's Monthly Tax Finder
 
-# The amount of county sales tax.
-# The amount of state sales tax.
-# The total sales tax (county plus state)
-# Use at least one function with input and output in this program
+#PSUEDO CODE:
+
+# 1) Set up Def function
+# 2) Set the tax rates (State_Tax = .05, County_Tax = .025)
+# 3) Set up formulas for taxes and total, return taxes/total
+# 4) Ask (float input) total monthly sales
+# 5) Calculate all the taxes
+# 6) Print all the taxes
+
+
+# 1)
+def calculate(total_sales):
+# 2)
+    S_T_R = .05
+    C_T_R = .025
+
+# 3)
+    state_tax = total_sales * S_T_R
+    county_tax = total_sales * C_T_R
+    total_tax = state_tax + county_tax
+
+    return state_tax, county_tax, total_tax
+
+# 4)
+sales = float(input("How much did you make in monthly sales? "))
+
+# 5)
+state_tax, county_tax, total_tax = calculate(sales)
+
+# 6)
+print(f"The state tax is ${state_tax:.2f}")
+print(f"The county tax is ${county_tax:.2f}")
+print(f"The total sales tax is ${total_tax:.2f}")
